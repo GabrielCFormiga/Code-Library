5bdad4d3367ebb5516770fbf51cb95f39745a41b
GabrielCFormiga <gabriel.campfor@gmail.com>
2026-06-13 09:14:10 -0300

    refactor(SegTree): fix type consistency in build, update, and query methods

--- Data Structures/Segment Tree.cpp
+++ Data Structures/Segment Tree.cpp
@@ -18,28 +18,28 @@ struct SegTree {
         build(1, 0, n - 1, a);
     }
 
-    void build(int pos, int tl, int tr, vector<ll> &a) {
-        if(tl == tr) { t[pos] = node(a[tl]); return; }
+    void build(int pos, int tl, int tr, const vector<ll> &a) {
+        if(tl == tr) { t[pos] = Node(a[tl]); return; }
         int tm = (tl + tr) / 2;
         build(2 * pos, tl, tm, a);
         build(2 * pos + 1, tm + 1, tr, a);
         t[pos] = t[2 * pos] + t[2 * pos + 1];
     }
 
-    void update(int i, int new_val, int pos, int tl, int tr) {
-        if(tl == tr) { t[pos] = node(new_val); return; }
+    void update(int i, ll new_val, int pos, int tl, int tr) {
+        if(tl == tr) { t[pos] = Node(new_val); return; }
         int tm = (tl + tr) / 2;
         if(i <= tm) update(i, new_val, 2 * pos, tl, tm);
         else update(i, new_val, 2 * pos + 1, tm + 1, tr);
         t[pos] = t[2 * pos] + t[2 * pos + 1];
     }
-    void update(int i, int new_val) { update(i, new_val, 1, 0, n - 1); }
+    void update(int i, ll new_val) { update(i, new_val, 1, 0, n - 1); }
 
-    node query(int l, int r, int pos, int tl, int tr) {
-        if(r < tl || tr < l) return node();
+    Node query(int l, int r, int pos, int tl, int tr) {
+        if(r < tl || tr < l) return Node();
         if(l <= tl && tr <= r) return t[pos];
         int tm = (tl + tr) / 2;
         return query(l, r, 2 * pos, tl, tm) + query(l, r, 2 * pos + 1, tm + 1, tr);
     }
-    node query(int l, int r) { return query(l, r, 1, 0, n - 1); }
+    Node query(int l, int r) { return query(l, r, 1, 0, n - 1); }
 };
